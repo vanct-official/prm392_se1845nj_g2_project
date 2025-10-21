c0a078339cf47f1092c5e95c0156ef992cb17d49
TranTuan66 <188287043+TranTuan-ops@users.noreply.github.com>
2025-10-22 05:06:28 +0700

    fix

--- app/src/main/java/com/example/finalproject/activity/admin/AddTourAdminActivity.java
+++ app/src/main/java/com/example/finalproject/activity/admin/AddTourAdminActivity.java
@@ -415,7 +415,7 @@ public class AddTourAdminActivity extends AppCompatActivity {
                 tour.put("guideIds", selectedGuideIds);
                 tour.put("images", imageUrls);
                 tour.put("status", status);
-                tour.put("createdAt", new Timestamp(new Date()));
+//                tour.put("createdAt", new Timestamp(new Date()));
 
                 // Lưu vào Firestore
                 db.collection("tours")
